b5641d1773227b3683c8300bbdf089b32f77e990
MaKo114 <67015114@kmitl.ac.th>
2025-08-13 16:39:06 +0700

    edit command

---
+++ server/mockup.py
@@ -0,0 +1,151 @@
+import aiohttp
+from bs4 import BeautifulSoup
+import re
+import random
+import csv
+from datetime import datetime, timedelta
+
+cache_expiration = timedelta(minutes=5)
+cache_data = None
+cache_timestamp = None
+
+########## SCAP WEB ##########
+
+async def fetch_real_time_data():
+    global cache_data, cache_timestamp
+    
+    if cache_data and cache_timestamp and datetime.now() - cache_timestamp < cache_expiration:
+        return cache_data
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://th.wikipedia.org/wiki/รายชื่อภาพยนต์ไทย') as response:
+           
+
+            return response
+
+# async def artist_song_image():
+#     h3_list, span_list, img_to_append = await fetch_real_time_data()
+    
+#     x = []
+#     for i in range(len(h3_list)):
+#         x.append({'Artist' : span_list[i].string.strip(),
+#                 'Song' : h3_list[i].string.strip(),
+#                 'Image' : img_to_append[i]})
+
+#     return x
+
+# ########## MANAGE DATA ##########
+
+# async def All_Songs():
+#     data = await artist_song_image()
+#     allSong = []
+#     song_id = 1
+
+#     for rank, song in enumerate(data, start=1):
+#         song_info = {
+#             'id': song_id,
+#             'artist': song['Artist'],
+#             'song': song['Song'],
+#             'img': song['Image'],
+#             'rank': str(rank)
+#         }
+        
+#         allSong.append(song_info)
+#         song_id += 1
+
+#     return allSong
+
+# async def All_Artist():
+#     allSong = await All_Songs()
+#     artist_data = {}
+    
+#     artist_id = 1
+#     for song in allSong:
+#         artist_name = song['artist']
+#         if artist_name not in artist_data:
+#             artist_data[artist_name] = {
+#                 'artistID': artist_id,
+#                 'artistName': artist_name,
+#                 'artistImg' : '',
+#                 'artistSong': []
+#             }
+#             artist_id += 1
+        
+#         artist_data[artist_name]['artistSong'].append({
+#             'id': song['id'],
+#             'song': song['song'],
+#             'img': song['img'],
+#             'rank': song['rank']
+#         })
+
+#     for artist_name, artist_info in artist_data.items():
+#         artist_songs = artist_info['artistSong']
+#         random_song = random.choice(artist_songs)
+#         artist_info['artistImg'] = random_song['img']
+
+#     artist_data = sorted(artist_data.values(), key=lambda x: x['artistName'])
+
+#     return artist_data
+
+# async def search_songs_by_artist(artist_name):
+#     allSong = await All_Songs()
+    
+#     artist_songs = {
+#         'artistID': None,
+#         'artistName': artist_name,
+#         'artistSong': []
+#     }
+
+#     artist_id = 1
+#     artist_found = False
+#     for song in allSong:
+#         if '-' in artist_name:
+#             artist_name = artist_name.replace('-', '/')
+
+#         if song['artist'] == artist_name:
+#             if not artist_found:
+#                 artist_songs['artistID'] = artist_id
+#                 artist_found = True
+#             artist_songs['artistSong'].append({
+#                 'id': song['id'],
+#                 'song': song['song'],
+#                 'img': song['img'],
+#                 'rank': song['rank']
+#             })
+#             artist_id += 1
+
+#     return artist_songs
+
+# async def search_songs_by_keyword(keyword):
+#     data = await All_Songs()
+
+#     search_results = []
+#     for song_info in data:
+#         if keyword.lower() in song_info['artist'].lower() or keyword.lower() in song_info['song'].lower():
+#             search_results.append(song_info)
+
+#     if not search_results:
+#         return "Not Found"
+    
+#     return search_results
+
+# async def export_to_csv(filename):
+#     data = await All_Songs()
+
+#     fieldnames = ['Rank', 'SongName', 'ArtistName', 'SongImg']
+
+#     with open(filename, mode='w', newline='', encoding='utf-8') as file:
+#         writer = csv.DictWriter(file, fieldnames=fieldnames)
+#         writer.writeheader()
+
+#         for song in data:
+#             writer.writerow({
+#                 'Rank': song['rank'],
+#                 'SongName': song['song'],
+#                 'ArtistName': song['artist'],
+#                 'SongImg': song['img'],
+#             })
+
+
+data =  fetch_real_time_data()
+print(data)
